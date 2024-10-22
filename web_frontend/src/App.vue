3ee27044b50deea13ce7b8b8e7c7f4b63f760574
QuanHertz-pku <aigohz985211@gmail.com>
2024-10-22 16:34:29 +0800

    try connection between frontend and backend

--- web_frontend/src/App.vue
+++ web_frontend/src/App.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>hello</div>
+  <div>
+    <div>username:{{ username }}</div>
+  </div>
   <router-view/>
 </template>
+
+<script>
+
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default {
+  name:"App",
+  setup() {
+    let username = ref("");
+
+    $.ajax({
+      url : "http://127.0.0.1:8081/try/get/",
+      type : "get",
+      success: resp => {
+        //console.log(resp);
+        username.value=resp;
+      }
+    });
+    return {username}
+  },
+}
+</script>
+
 
 <style>
 

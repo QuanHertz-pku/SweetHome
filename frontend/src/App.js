491d003e2b2ddac3d5b89ce0b5f43211de7ab49c
QuanHertz-pku <aigohz985211@gmail.com>
2024-11-07 16:14:52 +0800

    sort

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';  //使用jsx语法不需要引入react，但是如果使用了React下面的API，那么还是乖乖引入React吧
 import { Route, Routes } from 'react-router-dom';
-import { StartPage,LoginPage,HomePage } from './pages';
+import { LoginPage,HomePage } from './pages';
 
 function App() {
   return (

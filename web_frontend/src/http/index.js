6fc60652118c745358ac485ee914e91bfb6dde39
QuanHertz-pku <aigohz985211@gmail.com>
2024-11-07 15:26:31 +0800

    reset

--- web_frontend/src/http/index.js
+++
@@ -1,5 +0,0 @@
-import Sender from './Sender';
-
-let sender = new Sender();
-
-export default sender;
--- web_frontend/src/services/TryService/index.js
+++
@@ -1,5 +0,0 @@
-import TryService from "./TryService";
-
-let tryService = new TryService();
-
-export default tryService;

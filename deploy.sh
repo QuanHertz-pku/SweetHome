5e398c0bb9ee75d6d96f0efd0c97883eb9c93674
QuanHertz-pku <aigohz985211@gmail.com>
2024-11-10 16:37:24 +0800

    use!

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 克隆代码仓库
+cd SweetHome
+git pull
+# 执行部署操作
+docker-compose stop frontend backend
+docker-compose rm -f frontend backend
+docker-compose up -d frontend backend
+
+#配置Nginx
+docker cp default.conf frontend:/etc/nginx/conf.d/default.conf

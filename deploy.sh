69ed5ce579cc4a1d7a1b95506374ca5ce278e7af
QuanHertz-pku <aigohz985211@gmail.com>
2024-11-23 20:17:42 +0800

    fix bug of cache

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 # 执行部署操作
 docker-compose stop frontend backend
 docker-compose rm -f frontend backend
-docker-compose build frontend backend #天杀！如果不重建镜像Push一万遍都没效果qwq
-docker image prune -f #清理一下无效的卷！
+docker-compose build --no-cache frontend backend #天杀！如果不重建镜像Push一万遍都没效果qwq
+docker image prune -f #清理一下无效的镜像
 docker-compose up -d frontend backend
 
 #配置Nginx
